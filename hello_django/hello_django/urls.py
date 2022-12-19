bc4d2294d00d3ce893a3e8a17acc7f2a1cd01526
alexsenokosov <alexsenokosov666@gmail.com>
2022-12-19 21:55:13 +0300

    start_files_2

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -1,9 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.about),
     path('home/', views.home),
+    path('reverse/', views.reverse, name ='reversed'),
 ]
--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 
 
 def about(request):
-    a=200+200
+    a = 5+6
+    return render(request,'about.html',{'gretting':a})
 
-    return render(request,'about.html',{"gretting":a})
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request,'reverse.html',{'word':reverse})
+
 def home(request):
-    return HttpResponse('This is my h ome')
+    return HttpResponse('This is my home')
